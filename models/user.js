4fcf4625369b8a329ad55c5c2d71780d279c389d
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-20 09:57:16 +0530

    user model created

---
+++ models/user.js
@@ -0,0 +1,21 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema=newSchema({
+    email:{
+        type: String,
+        unique: [true, 'Email already exists'],
+        required: [true, 'Email is required']
+    },
+    username: {
+        type: String,
+        required: [true,'Username os required'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-z0-9._]+(?<![_.])$/,'Username invalid, it should contain 8-20 alphanumeric characters and be unique!']
+    },
+    image: {
+        type: String,
+    }
+})
+
+const User_promptopia=models.User_promptopia || model("User_promptomia",UserSchema)
+
+export default User_promptopia
