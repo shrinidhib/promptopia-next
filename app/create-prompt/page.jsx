dd8d77e469eca8e59d32ca89ad2211a853f63e8f
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-23 21:12:10 +0530

    created created form frontend

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -13,6 +13,27 @@ const CreatePrompt = () => {
     })
 
     const createPrompt=async(e)=>{
+        e.preventDefault()
+
+        setSubmitting(true)
+            const response=await fetch('/api/prompt/new',{
+                method:'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag,
+                })
+            })
+            if (response.ok){
+                router.push('/')
+            }
+        try{
+
+        }catch(error){
+            console.log(error)
+        }finally{
+            setSubmitting(false)
+        }
 
     }
   return (
