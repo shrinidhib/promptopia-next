e6c96e24df3afbd9eb694144e4dfeefea3ca3b6a
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-24 09:52:10 +0530

    search box in feed created

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+
+import PromptCard from './PromptCard'
+
+
 
 const Feed = () => {
+  const [searchText, setSearchText]=useState('')
+  const handleSearchChange=(e)=>{
+
+  }
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+        type='text'
+        placeholder='search for a tag or a username'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className='search_input peer'/>
+
+      </form>
+      <PromptCardList
+      data={[]}
+      handleTagClick={()=>{}}/>
+
+    </section>
   )
 }
 
