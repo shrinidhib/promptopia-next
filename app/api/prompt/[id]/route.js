a1bd9c601c1444b50619621b6fe4a55959d98303
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-24 12:16:40 +0530

    update prompt route established

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -15,6 +15,29 @@ export const GET=async(request,{params})=>{
         })
 
     }catch(error){
-        return new Response("failed to fetch all prompts",{status:500})
+        return new Response("failed to fetch prompt",{status:500})
+    }
+}
+
+export const PATCH= async (request,{params})=>{
+    const {prompt,tag}=await request.json()
+    try{
+        await connectToDB()
+        const existingPrompt=await Prompt.findById(params.id)
+
+        if (!existingPrompt){
+            return new Response("Prompt not found",{status:404})
+        }
+        existingPrompt.prompt=prompt;
+        existingPrompt.tag=tag;
+
+        await existingPrompt.save()
+
+        return new Response(JSON.stringify(existingPrompt),{
+            status:200
+        })
+
+    }catch(error){
+        return new Response("failed to update prompt",{status: 500})
     }
 }
