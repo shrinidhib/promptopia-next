15f3749b70d08afbdcd6b43b076484e93b766acf
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-24 11:21:13 +0530

    promptcard created and copy to clipboard established

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,19 +18,18 @@ const PromptCardList=({data,handleTagClick})=>{
 const Feed = () => {
   const [searchText, setSearchText]=useState('')
   const [posts,setPosts]=useState([])
-  const handleSearchChange=(e)=>{
-
-
-    useEffect(()=>{
-      const fetchPosts=async()=>{
-        const response=await fetch('/api/prompt')
-         const data=await response.json()
-
-        setPosts(data)
-      }
-      fetchPosts()
-    },[])
+  const handleSearchChange=(e)=>{ 
   }
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      const response=await fetch('/api/prompt')
+      const data=await response.json()
+      console.log(data)
+
+      setPosts(data)
+    }
+    fetchPosts()
+  },[])
   return (
     <section className='feed'>
       <form className='relative w-full flex-center'>
