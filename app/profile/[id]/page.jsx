8698cb84398837f67c324f476c59dffc9328dfdf
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-24 13:06:47 +0530

    see other user's profile functionality added

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,32 @@
+"use client"
+import React, { useState, useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
+
+import Profile from '@components/Profile'
+
+    
+
+const UserProfile = ({params}) => {
+    const searchParams=useSearchParams()
+    const userName=searchParams.get("name")
+    const [userPosts,setUserPosts]=useState([])
+   
+    useEffect(()=>{
+        const fetchPosts=async()=>{
+          const response=await fetch(`/api/users/${params?.id}/posts`)
+          const data=await response.json()
+          console.log(data)
+    
+          setUserPosts(data)
+        }
+        if (params?.id) {
+            fetchPosts()}
+      },[params.id])
+  return (
+    <Profile name={userName}
+    desc={`Welcome to ${userName}'s profle page`}
+    data={userPosts}/>
+  )
+}
+
+export default UserProfile
